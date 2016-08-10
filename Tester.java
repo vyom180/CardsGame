30b7c60517421c798718f7403787a319d79fcba1
vyom180 <vyom.rastogi.ece12@itbhu.ac.in>
2016-08-10 09:14:17 +0530

    Update Tester.java
    All functions

--- Tester.java
+++ Tester.java
@@ -5,24 +5,104 @@ public class Tester {
 	public static void main(String[] args) {
 		Hand hand = new Hand();
 		
-		// Checking Straight Flush
-//		ArrayList<Card> cards = new ArrayList<Card>();
-//		cards.add(new Card(Suits.HEARTS,Denom.ACE));
-//		cards.add(new Card(Suits.HEARTS,Denom.TWO));
-//		cards.add(new Card(Suits.HEARTS,Denom.THREE));
-//		cards.add(new Card(Suits.HEARTS,Denom.FOUR));
-//		cards.add(new Card(Suits.HEARTS,Denom.FIVE));
-//		hand.setCards(cards);
-//		p(hand.evaluatePoker().toString()+"\n");
+		//Checking Straight Flush
+		ArrayList<Card> cards = new ArrayList<Card>();
+		cards.add(new Card(Suits.HEARTS, Denom.ACE));
+		cards.add(new Card(Suits.HEARTS, Denom.TWO));
+		cards.add(new Card(Suits.HEARTS, Denom.THREE));
+		cards.add(new Card(Suits.HEARTS, Denom.FOUR));
+		cards.add(new Card(Suits.HEARTS, Denom.FIVE));
+		hand.setCards(cards);
+		p(hand.evaluatePoker().toString()+"\n");
 		
+		cards.clear();
 		// Checking Royal Flush
-		ArrayList<Card> cards1 = new ArrayList<Card>();
-		cards1.add(new Card(Suits.HEARTS,Denom.ACE));
-		cards1.add(new Card(Suits.HEARTS,Denom.KING));
-		cards1.add(new Card(Suits.HEARTS,Denom.QUEEN));
-		cards1.add(new Card(Suits.HEARTS,Denom.JACK));
-		cards1.add(new Card(Suits.HEARTS,Denom.TEN));
-		hand.setCards(cards1);
+		cards.add(new Card(Suits.HEARTS, Denom.ACE));
+		cards.add(new Card(Suits.HEARTS, Denom.KING));
+		cards.add(new Card(Suits.HEARTS, Denom.QUEEN));
+		cards.add(new Card(Suits.HEARTS, Denom.JACK));
+		cards.add(new Card(Suits.HEARTS, Denom.TEN));
+		hand.setCards(cards);
+		p(hand.evaluatePoker().toString()+"\n");
+		
+		cards.clear();
+		//Checking Four of a kind
+		cards.add(new Card(Suits.CLUBS, Denom.ACE));
+		cards.add(new Card(Suits.HEARTS, Denom.ACE));
+		cards.add(new Card(Suits.SPADES, Denom.ACE));
+		cards.add(new Card(Suits.DIAMONDS, Denom.ACE));
+		cards.add(new Card(Suits.HEARTS, Denom.TEN));
+		hand.setCards(cards);
+		p(hand.evaluatePoker().toString()+"\n");
+		
+		cards.clear();
+		//Checking Full House
+		cards.add(new Card(Suits.CLUBS, Denom.ACE));
+		cards.add(new Card(Suits.HEARTS, Denom.ACE));
+		cards.add(new Card(Suits.SPADES, Denom.ACE));
+		cards.add(new Card(Suits.DIAMONDS, Denom.TEN));
+		cards.add(new Card(Suits.HEARTS, Denom.TEN));
+		hand.setCards(cards);
+		p(hand.evaluatePoker().toString()+"\n");
+		
+		cards.clear();
+		//Checking Flush
+		cards.add(new Card(Suits.HEARTS, Denom.TEN));
+		cards.add(new Card(Suits.HEARTS, Denom.KING));
+		cards.add(new Card(Suits.HEARTS, Denom.EIGHT));
+		cards.add(new Card(Suits.HEARTS, Denom.JACK));
+		cards.add(new Card(Suits.HEARTS, Denom.TWO));
+		hand.setCards(cards);
+		p(hand.evaluatePoker().toString()+"\n");
+		
+		cards.clear();
+		//Checking Straight
+		cards.add(new Card(Suits.HEARTS, Denom.TWO));
+		cards.add(new Card(Suits.HEARTS, Denom.THREE));
+		cards.add(new Card(Suits.HEARTS, Denom.FOUR));
+		cards.add(new Card(Suits.HEARTS, Denom.FIVE));
+		cards.add(new Card(Suits.HEARTS, Denom.SIX));
+		hand.setCards(cards);
+		p(hand.evaluatePoker().toString()+"\n");
+		
+		cards.clear();
+		//Checking Three Of a Kind
+		cards.add(new Card(Suits.CLUBS, Denom.ACE));
+		cards.add(new Card(Suits.HEARTS, Denom.ACE));
+		cards.add(new Card(Suits.SPADES, Denom.ACE));
+		cards.add(new Card(Suits.DIAMONDS, Denom.TEN));
+		cards.add(new Card(Suits.HEARTS, Denom.TWO));
+		hand.setCards(cards);
+		p(hand.evaluatePoker().toString()+"\n");
+		
+		cards.clear();
+		//Checking Two Pair
+		cards.add(new Card(Suits.CLUBS, Denom.ACE));
+		cards.add(new Card(Suits.HEARTS, Denom.ACE));
+		cards.add(new Card(Suits.SPADES, Denom.TWO));
+		cards.add(new Card(Suits.DIAMONDS, Denom.TEN));
+		cards.add(new Card(Suits.HEARTS, Denom.TEN));
+		hand.setCards(cards);
+		p(hand.evaluatePoker().toString()+"\n");
+		
+		cards.clear();
+		//Checking One Pair
+		cards.add(new Card(Suits.CLUBS, Denom.ACE));
+		cards.add(new Card(Suits.HEARTS, Denom.ACE));
+		cards.add(new Card(Suits.SPADES, Denom.TWO));
+		cards.add(new Card(Suits.DIAMONDS, Denom.THREE));
+		cards.add(new Card(Suits.HEARTS, Denom.TEN));
+		hand.setCards(cards);
+		p(hand.evaluatePoker().toString()+"\n");
+		
+		cards.clear();
+		//Checking Full House
+		cards.add(new Card(Suits.CLUBS, Denom.ACE));
+		cards.add(new Card(Suits.HEARTS, Denom.KING));
+		cards.add(new Card(Suits.SPADES, Denom.EIGHT));
+		cards.add(new Card(Suits.DIAMONDS, Denom.TWO));
+		cards.add(new Card(Suits.HEARTS, Denom.TEN));
+		hand.setCards(cards);
 		p(hand.evaluatePoker().toString()+"\n");
 		
 	}
